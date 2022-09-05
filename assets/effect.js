2387990157b86f8b523b781bca879c30603257e8
XavierMaYuqian <32877925+XavierMaYuqian@users.noreply.github.com>
2022-09-05 17:18:05 +0800

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -124,20 +124,6 @@ $('document').ready(function(){
 		});
 	});	
 
-	$('#cake_fadein').click(function(){
-		$('.cake').fadeIn('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#light_candle').fadeIn('slow');
-		});
-	});
-
-	$('#light_candle').click(function(){
-		$('.fuego').fadeIn('slow');
-		$(this).fadeOut('slow').promise().done(function(){
-			$('#wish_message').fadeIn('slow');
-		});
-	});
-
 		
 	$('#wish_message').click(function(){
 		 vw = $(window).width()/2;
